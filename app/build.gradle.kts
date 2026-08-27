b39fae8700171e8977d4178df37c7ab30e865d3d
fuchuanboy <fuchuanboy@gmail.com>
2026-08-27 22:49:02 +0800

    Add Android app Gradle configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.application") version "8.7.3"
+    id("org.jetbrains.kotlin.android") version "2.0.21"
+}
+
+android { namespace="com.fuchuanboy.todaylunch"; compileSdk=35
+    defaultConfig { applicationId="com.fuchuanboy.todaylunch"; minSdk=23; targetSdk=35; versionCode=1; versionName="1.0" }
+}
