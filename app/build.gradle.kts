386fbb4ac6bd68be0aeac453e813bddc2639829f
fuchuanboy <fuchuanboy@gmail.com>
2026-08-27 23:12:51 +0800

    Fix Android Java/Kotlin JVM target compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,24 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.21"
 }
 
-android { namespace="com.fuchuanboy.todaylunch"; compileSdk=35
-    defaultConfig { applicationId="com.fuchuanboy.todaylunch"; minSdk=23; targetSdk=35; versionCode=1; versionName="1.0" }
+android {
+    namespace = "com.fuchuanboy.todaylunch"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.fuchuanboy.todaylunch"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
